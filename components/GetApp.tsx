23e131491727107f00a4617174ceae56b478a9d6
Priyans4475 <priyanshukunar007@gmail.com>
2023-12-26 17:19:25 +0530

    first commit

---
+++ components/GetApp.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Button from './Button'
+import Image from 'next/image'
+
+const GetApp = () => {
+  return (
+    <section className='flexcenter w-full   flex-col pb-[100px] lg:rounded-full border '>
+      <div className='get-app'>
+        <div className='z-20 flex w-full flex-1 flex-col items-start
+        justify-center gap-12 '>
+          <h2 className='bold-40 lg:bold-64 xl:max-w-[320px]'>
+            Get for free now!
+          </h2>
+          <p className='regular-16 text-gray-10'>
+            Available in ios and Android
+
+          </p>
+
+        <div className='flex w-full flex-col gap-3
+        whitespace-nowrap xl:flex-row'>
+         <Button
+         type='button'
+         title='App store'
+         icon='/apple.svg'
+         variant='btn_white'
+         full
+
+         />
+          <Button
+         type='button'
+         title='Play store'
+         icon='/android.svg'
+         variant='btn_dark_green_outline'
+         full
+
+         />
+         </div>
+         </div>
+      <div className='flex flex-1 items-center justify-end lg:w-full '>
+        <Image 
+        src="/phones.png"
+        alt="phones"
+        width={550}
+        height={870}
+        />
+
+      </div>
+
+      </div>
+       
+    </section>
+  )
+}
+
+export default GetApp
